0bcd3176b921a23955526f1873af8ea070ae5f88
soffiaazzahra <118706738+soffiaazzahra@users.noreply.github.com>
2022-11-29 15:25:26 +0700

    Add files via upload

---
+++ soffia.js
@@ -0,0 +1,4 @@
+var input = require('readline-sync')
+var panjang = input.question(" masukkan panjang persegi panjang =");
+var lebar = input.question(" masukkan lebar persegi panjang =");
+console.log("hasil luas persegi panjang adalah", panjang  * lebar , "cm kubik");
